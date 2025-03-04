b1639609bacbfb5e3d109a4561fa9ff9889d6ead
Charles Farris <charlesfarris71@gmail.com>
2025-03-03 23:58:55 -0700

    Update MySql tests

--- src/MySql.Tests/TemporaryDatabaseGuardTests.cs
+++ src/MySql.Tests/TemporaryDatabaseGuardTests.cs
@@ -1,25 +1,43 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using MySql.Data.MySqlClient;
 
 namespace SleepingBear.TemporaryDatabase.MySql.Tests;
 
 /// <summary>
 ///     Tests for <see cref="TemporaryDatabaseGuard" />.
 /// </summary>
-[SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task")]
 internal static class TemporaryDatabaseGuardTests
 {
     [Test]
     public static async Task FromConnectionStringAsync_ValidatesBehavior()
     {
         var connectionString = Environment.GetEnvironmentVariable("SBS_TEST_SERVER_MYSQL");
-        await using var guard = await TemporaryDatabaseGuard.FromConnectionStringAsync(connectionString);
-        Assert.That(guard.ConnectionString, Is.Not.Null);
+        var guard = await TemporaryDatabaseGuard
+            .FromConnectionStringAsync(
+                connectionString,
+                new DatabaseOptions
+                {
+                    SslMode = MySqlSslMode.Disabled
+                })
+            .ConfigureAwait(false);
+        await using (guard.ConfigureAwait(false))
+        {
+            Assert.That(guard.ConnectionString, Is.Not.Null);
+        }
     }
 
     [Test]
     public static async Task FromEnvironmentVariableAsync_ValidatesBehavior()
     {
-        await using var guard = await TemporaryDatabaseGuard.FromEnvironmentVariableAsync();
-        Assert.That(guard.ConnectionString, Is.Not.Null);
+        var guard = await TemporaryDatabaseGuard
+            .FromEnvironmentVariableAsync(
+                options: new DatabaseOptions
+                {
+                    SslMode = MySqlSslMode.Disabled
+                })
+            .ConfigureAwait(false);
+        await using (guard.ConfigureAwait(false))
+        {
+            Assert.That(guard.ConnectionString, Is.Not.Null);
+        }
     }
 }
